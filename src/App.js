902a55655ef4a0f4cd706fa3f9ac66f4b0527031
blazingvx <backupworkvx@gmail.com>
2023-09-24 01:01:49 +0530

    Update details

--- src/App.js
+++ src/App.js
@@ -7,44 +7,47 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <header className="header">
-          <h1 className="name">John Doe</h1>
-          <p className="job-title">GIS Desktop Developer</p>
+          <h1 className="name">Vedant Sood</h1>
+          <p className="job-title">GIS Developer & Data Analyst </p>
         </header>
-
+        <hr
+        style={{
+          background: 'lime',
+          color: 'lime',
+          borderColor: 'lime',
+          height: '3px',
+        }}
+      />
         <nav className="menu">
-          <ul>
-            <li>
-              <a href="#summary">Summary</a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#experience">Experience</a>
-            </li>
-            <li>
-              <a href="#contact-info">Contact</a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/your-linkedin-profile"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/your-github-profile"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </li>
-          </ul>
-        </nav>
+  <ul>
+    <li>
+      <a href="#summary">Summary</a>
+    </li>
+    <li>
+      <a href="#skills">Skills</a>
+    </li>
+    <li>
+      <a href="#technical_skills">Technical Skills</a>
+    </li>
+    <li>
+      <a href="#experience">Experience</a>
+    </li>
+    <li>
+      <a href="#projects">Projects</a>
+    </li>
+    <li>
+      <a href="https://www.linkedin.com/in/vedant-sood" target="_blank" rel="noopener noreferrer">
+        LinkedIn
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/blazingvx" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+    </li>
+  </ul>
+</nav>
+
       </div>
 
       <main className="main">
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -22,53 +22,65 @@ const Resume = () => {
       <section id="skills" className="section">
         <h2>Skills</h2>
         <ul>
-          <li>GIS Software Development (e.g., ArcGIS, QGIS)</li>
-          <li>Geospatial Data Analysis</li>
-          <li>Python Programming</li>
-          <li>Database Management (SQL, PostgreSQL)</li>
-          <li>Cartography and Map Design</li>
-          <li>GeoJSON and Shapefile Manipulation</li>
-          <li>Desktop GIS Application Development</li>
+          <li>Data Structures</li>
+          <li>Geo-spatial Technology</li>
+          <li>Software Methodology</li>
+          <li>Algorithms Analysis</li>
+          <li>Database Management</li>
+          <li>Artificial Intelligence</li>
+          <li>Systems Programming</li>
+          <li>Computer Architecture</li>
         </ul>
       </section>
 
+      <section id="technical_skills" className="section">
+      <h2>Technical Skills</h2>
+      <ul>
+          <li>Languages: Java, Python, C#, C, HTML/CSS, JavaScript, SQL, Bat</li>
+          <li>Developer Tools: Visual Studio, VS Code, Google Cloud Platform, Android Studio</li>
+          <li>Technologies/Frameworks: Spring Boot, Linux, Jenkins, GitHub, ArcGIS Desktop/Server, ArcFM Desktop, Geo-database Manager, MS VISIO</li>
+        </ul>
+      </section>
       {/* Experience section */}
       <section id="experience" className="section">
         <h2>Experience</h2>
         <div className="experience-item">
-          <h3>GIS Desktop Developer</h3>
-          <p className="company">GeoTech Solutions Inc.</p>
-          <p className="date">June 2020 - Present</p>
+        <p className="company"><h3>Tata Consultancy Services - Information Technology Analyst (ITA)</h3></p>
+          <br/>
           <ul>
-            <li>Developed custom GIS desktop applications using ArcGIS API for JavaScript.</li>
-            <li>Designed and implemented geospatial data visualization and analysis tools.</li>
-            <li>Worked on GIS database management and optimization.</li>
-          </ul>
-        </div>
-
-        <div className="experience-item">
-          <h3>GIS Analyst</h3>
-          <p className="company">GeoData Services</p>
-          <p className="date">May 2017 - May 2020</p>
-          <ul>
-            <li>Conducted geospatial analysis and produced thematic maps for clients.</li>
-            <li>Assisted in GIS data collection and field surveys.</li>
-            <li>Developed Python scripts for automating geoprocessing tasks.</li>
+            <li>Led migration of various software solutions to cloud platforms.</li>
+            <li>Managed complex database migrations.</li>
+            <li>Designed and improved workflows for enhanced efficiency.</li>
+            <li>Implemented robust security measures during code migration.</li>
+            <li>Developed utilities for seamless interaction between software interfaces.</li>
+            <li>Engineered middleware for third-party integrations.</li>
           </ul>
         </div>
       </section>
 
+      <section id="projects" className="section">
+        <h2>Projects</h2>
+        <div className="experience-item">
+        <p className="company"><h3>Software Transformation and Enhancement</h3></p> 
+        <br/> 
+          <ul>
+            <li>Upgraded and modernized legacy software applications.</li>
+            <li>Developed APIs for efficient change tracking.</li>
+            <li>Managed project tasks and backlog efficiently.</li>
+            <li>Designed user-friendly interfaces for diverse software solutions.</li>
+            <li>Worked on server-side development and optimized services.</li>
+            <li>Effectively handled database operations and performance tuning.</li>
+          </ul>
+        </div>
+      </section>
       {/* Contact Info section */}
       <section id="contact-info" className="section">
         <h2>Contact Info</h2>
         <p className="contact">
-          <span className="info-icon">&#9742;</span> (123) 456-7890
+          <span className="info-icon">&#9993;</span> vedantsood@gmail.com
         </p>
         <p className="contact">
-          <span className="info-icon">&#9993;</span> johndoe@email.com
-        </p>
-        <p className="contact">
-          <span className="info-icon">&#127968;</span> Anytown, USA
+          <span className="info-icon">&#127968;</span> New Delhi, India
         </p>
       </section>
     </div>
